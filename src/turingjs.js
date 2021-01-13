f33969438ba53b5eef91fd1965aa550a60adfdf6
tq9epa <48084414+tq9epa@users.noreply.github.com>
2021-01-13 17:24:42 +0100

    from vela

--- src/turingjs.js
+++ src/turingjs.js
@@ -23,30 +23,95 @@ $(function () {
     });
 
     $('#calc').click(function () {
-        var calc = prompt('Adj meg egy imput szalagot (pl.: 4!):');
-        if (calc) {
-            var match = calc.match(/([0-9]+)(!)/i);
-            if (match) {
-                var first = parseInt(match[1]);
-                var tape = [];
-                var i = 0;
-                for(k=first; k>=1; k--) {
-                    for(j=0; j<k; j++) {
-                        tape[i++] = '1';
-                    }
-                    if(k!=1) {
-                        tape[i++]='*';
-                    }
-                }
-                
-                
-                tape[i++] = '=';
-                turing.tape = tape;
-                turing.calc = first + '!' + ' =';
-                turing.reset();
-            } else {
-                alert('Rossz input!');
+        
+        switch (option) {
+            case "1":
+                var calc = prompt('Adj meg egy imput szalagot (e.g. 4!):');
+				if (calc) {
+					var match = calc.match(/([0-9]+)(!)/i);
+					if (match) {
+						var first = parseInt(match[1]);
+						var tape = [];
+						var i = 0;
+						for(k=first; k>=1; k--) {
+							for(j=0; j<k; j++) {
+								tape[i++] = '1';
+							}
+							if(k!=1) {
+								tape[i++]='*';
+							}
+						}
+						
+						
+						tape[i++] = '=';
+						turing.tape = tape;
+						turing.calc = first + '!' + ' =';
+						turing.reset();
+					} else {
+						alert('Valami nem jó!');
+					}
+				}
+                break;
+            case "2":
+                var calc = prompt('Adj meg egy imput szalagot (e.g. 4!):');
+				if (calc) {
+					var match = calc.match(/([0-9]+)(!)/i);
+					if (match) {
+						var first = parseInt(match[1]);
+						var tape = [];
+						var i = 0;
+						if(first==0) {
+							tape[i++] = '0';
+						} else {
+							for (j=0; j<first; j++) {
+								tape[i++] = '1';
+							}
+						}
+
+						tape[i++] = '!';
+						tape[i++] = '=';
+						turing.tape = tape;
+						turing.calc = first + '!' + ' =';
+						turing.reset();
+					} else {
+						alert('Valami nem jó!');
+					}
+				}
+                break;
+            case "3":
+                var calc = prompt('Adj meg egy imput szalagot(e.g. 3 * 2):');
+				if (calc) {
+					var match = calc.match(/([0-9]+)[ ]*\*[ ]*([0-9]+)/i);
+					if (match) {
+						var first = parseInt(match[1]), second = parseInt(match[2]);
+						var tape = [];
+						var i = 0;
+						if (0 == first) {
+							tape[i++] = '0';
+						} else {
+							for ( ; i < first; i++) {
+								tape[i] = '1';
+							}
+						}
+						tape[i++] = '*';
+						if (0 == second) {
+							tape[i++] = '0';
+						} else {
+							for (var end = second + i; i < end; i++) {
+								tape[i] = '1';
+							}
+						}
+						tape[i++] = '=';
+						turing.tape = tape;
+						turing.calc = first + ' * ' + second + ' =';
+						turing.reset();
+					} else {
+						alert('Valami nem jó!');
+					}
+				}
+                break;
+            
             }
-        }
+       
     });
 });
